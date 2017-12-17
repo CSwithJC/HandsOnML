f5b676302f0e368b92214d1643b888f4229212e1
jean28 <jean.mendez2@upr.edu>
2017-12-17 12:27:51 -0500

    Lifecycle of node values.

---
+++ chapter_09_getting_started_with_tensorflow/lesson_02_lifecycle_of_node_value.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+# Create a computational graph, but doesn't actually do any operations.
+w = tf.constant(3)
+x = w + 2
+y = x + 5
+z = x * 3
+
+with tf.Session() as sess:
+    print(y.eval())  # 10
+    print(z.eval())  # 15
+
+"""In the code above, TensorFlow detects that y depends on w,
+which depends on x, so it evaluates w first, then x, then y. However,
+the values for x and w are not reused, so it is evaluating w and x twice.
+
+To evaluate y and z efficiently, without evaluating w and x twice, you must
+ask TensorFlow to evaluate both y and z in just one graph run, as follows:
+"""
+
+with tf.Session() as sess:
+    y_val, z_val = sess.run([y, z])
+    print(y_val)  # 10
+    print(z_val)  # 15
+
+"""In single-process TensorFlow, multiple sessions do not share any state, even
+if they reuse the same graph. In distributed TensorFlow, variable state is stored
+on the servers, not in the sessions, so multiple sessions can share the same variables.
+"""
