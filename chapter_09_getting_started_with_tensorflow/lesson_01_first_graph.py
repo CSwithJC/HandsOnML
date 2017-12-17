865ee5025f2a29f77e6cdc831addb48529b221c4
jean28 <jean.mendez2@upr.edu>
2017-12-17 00:17:51 -0500

    Updated possible ways of creating TensorFlow session.

--- chapter_09_getting_started_with_tensorflow/lesson_01_first_graph.py
+++ chapter_09_getting_started_with_tensorflow/lesson_01_first_graph.py
@@ -6,10 +6,27 @@ y = tf.Variable(4, name="y")
 f = x * x * y + y + 2
 
 # Open a TensorFlow session to evaluate the graph created above.
+
+"""
+# An Option:
 sess = tf.Session()
 sess.run(x.initializer)
 sess.run(y.initializer)
 result = sess.run(f)
+
+# Another Option:
+with tf.Session() as sess:
+    sess.run(x.initializer)
+    sess.run(y.initializer)
+    result = sess.run(f)
+"""
+
+# Another option:
+init = tf.global_variables_initializer()  # prepare an init node
+with tf.Session() as sess:
+    init.run()  # actually initialize all the variables
+    result = f.eval()  # equivalent to calling tf.get_default_session().run(f)
+
 
 # See results from graph and close session.
 print(result)
