27fbcc225ee22063f6bae92a1370979a6f49e624
jean28 <jean.mendez2@upr.edu>
2017-12-18 22:27:17 -0500

    Tensorboard example.

---
+++ chapter_09_getting_started_with_tensorflow/lesson_08_tensorboard.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+import numpy as np
+from datetime import datetime
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+
+"""Example of using Tensorboard; remember to include timestamp in the log
+directory name.
+"""
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+
+# Add extra bias input feature (x0 = 1) to all training instances
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+# These are now placeholder nodes:
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+XT = tf.transpose(X)
+
+# Normalize the input feature vector or training may be much slower.
+scaler = StandardScaler()
+scaler.fit(housing_data_plus_bias)
+scaled_housing_data_plus_bias = scaler.transform(housing_data_plus_bias)
+
+# Theta are initially a lot of random variables between -1 and 1
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
+
+# Matrix Multiplication
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+learning_rate = 0.01
+
+# Gradient function:
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+init = tf.global_variables_initializer()
+
+# TENSORBOARD CODE:
+# Create unique name per logdir by appending timestamp
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = "tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
+
+# Creates a node in the graph that will evaluate the MSE value and write it
+# to a TensorBoard-compatible binary log string called a "summary".
+mse_summary = tf.summary.scalar('MSE', mse)
+
+# Creates a FileWriter used to write summaries to logfiles in the log directory.
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+n_epochs = 1000
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)
+    indices = np.random.randint(m, size=batch_size)
+    X_batch = scaled_housing_data_plus_bias[indices]
+    y_batch = housing.target.reshape(-1, 1)[indices]
+
+    return X_batch, y_batch
+
+
+with tf.Session() as sess:
+
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+
+file_writer.close()
+
+print("Best theta is: ")
+print(best_theta)
