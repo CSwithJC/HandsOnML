238ff7cc55ad214985c7f4ea4cdd338cc6733c40
jean28 <jean.mendez2@upr.edu>
2017-12-18 22:56:18 -0500

    Sharing Variables.

---
+++ chapter_09_getting_started_with_tensorflow/lesson_11_sharing_variables.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+
+"""Examples of different ways of sharing variables within TensorFlow:
+"""
+
+n_features = 3
+
+#
+# 1. Create variable first, and then pass it to relu:
+#
+def relu(X, threshold):
+    with tf.name_scope("relu"):
+        w_shape = (int(X.get_shape()[1]), 1)
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")
+        b = tf.Variable(0.0, name="bias")
+        z = tf.add(tf.matmul(X, w), b, name="z")
+        return tf.maximum(z, threshold, name="max")
+
+threshold = tf.Variable(0.0, name="threshold")
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X, threshold) for i in range(5)]
+output = tf.add_n(relus, name="output")
+
+#
+# 2. Set a shared variable as an attribute of the relu() function:
+#
+def relu(X):
+    with tf.name_scope("relu"):
+        if not hasattr(relu, "threshold"):
+            relu.threshold = tf.Variable(0.0, name="threshold")
+        w_shape = int(X.get_shape()[1]), 1
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")
+        b = tf.Variable(0.0, name="bias")
+        z = tf.add(tf.matmul(X, w), b, name="z")
+        return tf.maximum(z, relu.threshold, name="max")
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name="output")
+
+#
+# 3. Using get_variable:
+#
+with tf.variable_scope("relu"):
+    threshold = tf.get_variable("threshold", shape=(),
+                                initializer=tf.constant_initializer(0.0))
+
+with tf.variable_scope("relu", reuse=True):
+    threshold = tf.get_variable("threshold")
+
+with tf.variable_scope("relu") as scope:
+    scope.reuse_variables()
+    threshold = tf.get_variable("threshold")
+
+
+#
+# Another Way
+#
+def relu(X):
+    with tf.variable_scope("relu"):
+        threshold = tf.get_variable("threshold", shape=(), initializer=tf.constant_initializer(0.0))
+        w_shape = (int(X.get_shape()[1]), 1)
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")
+        b = tf.Variable(0.0, name="bias")
+        z = tf.add(tf.matmul(X, w), b, name="z")
+        return tf.maximum(z, threshold, name="max")
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+with tf.variable_scope("", default_name="") as scope:
+    first_relu = relu(X)     # create the shared variable
+    scope.reuse_variables()  # then reuse it
+    relus = [first_relu] + [relu(X) for i in range(4)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu8", tf.get_default_graph())
+file_writer.close()
+
+#
+# Another Way
+#
+def relu(X):
+    threshold = tf.get_variable("threshold", shape=(),
+                                initializer=tf.constant_initializer(0.0))
+    w_shape = (int(X.get_shape()[1]), 1)                        # not shown in the book
+    w = tf.Variable(tf.random_normal(w_shape), name="weights")  # not shown
+    b = tf.Variable(0.0, name="bias")                           # not shown
+    z = tf.add(tf.matmul(X, w), b, name="z")                    # not shown
+    return tf.maximum(z, threshold, name="max")
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = []
+for relu_index in range(5):
+    with tf.variable_scope("relu", reuse=(relu_index >= 1)) as scope:
+        relus.append(relu(X))
+output = tf.add_n(relus, name="output")
